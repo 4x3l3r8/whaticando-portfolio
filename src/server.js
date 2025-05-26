07cac55c0ec01081796ba307830237ae2f6a2bce
Daniel Adesanya <4x3l3r8@gmail.com>
2025-05-26 15:24:11 +0100

    fix bug

--- src/server.js
+++ src/server.js
@@ -9,10 +9,10 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
+void app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+    void handle(req, res, parsedUrl);
   }).listen(process.env.PORT ?? 3000, (err) => {
     if (err) throw err;
     console.log("> Ready on http://localhost:3000");
