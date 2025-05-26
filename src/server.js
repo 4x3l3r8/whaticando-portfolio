cf4ba68c88a64f514573d2bc9a14fb49c8a386aa
Daniel Adesanya <4x3l3r8@gmail.com>
2025-05-26 15:22:21 +0100

    trial push

--- src/server.js
+++ src/server.js
@@ -9,10 +9,10 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-void app.prepare().then(() => {
+app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
-    void handle(req, res, parsedUrl);
+    handle(req, res, parsedUrl);
   }).listen(process.env.PORT ?? 3000, (err) => {
     if (err) throw err;
     console.log("> Ready on http://localhost:3000");
